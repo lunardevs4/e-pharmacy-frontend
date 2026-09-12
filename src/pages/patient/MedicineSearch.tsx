dedaacb735352ef88d9da4a4fca8bbf72260979a
Prisca <iyonezalarissaprisca@gmail.com>
2026-09-12 20:09:58 +0200

    Live navigation

--- src/pages/patient/MedicineSearch.tsx
+++ src/pages/patient/MedicineSearch.tsx
@@ -651,10 +651,7 @@ export default function MedicineSearch() {
         >
           <iframe
             title="Google Map Locator"
-            src={(userLocation && mapQuery !== 'Kigali, Rwanda' && mapQuery !== `${userLocation.lat},${userLocation.lng}`) 
-              ? `https://maps.google.com/maps?saddr=${userLocation.lat},${userLocation.lng}&daddr=${encodeURIComponent(mapQuery)}&t=m&z=${mapZoom}&output=embed`
-              : `https://maps.google.com/maps?q=${encodeURIComponent(mapQuery)}&t=m&z=${mapZoom}&output=embed`
-            }
+            src={`https://maps.google.com/maps?q=${encodeURIComponent(mapQuery)}&t=m&z=${mapZoom}&output=embed`}
             className="w-full h-full min-h-[300px] lg:min-h-full border-0 shadow-sm"
             allowFullScreen
             loading="lazy"
